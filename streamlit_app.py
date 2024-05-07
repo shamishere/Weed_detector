18ec0fb967c5d2c64f27f31f881db4c8571fdfde
ShameenAnwar <sham17anwar@gmail.com>
2024-05-08 01:14:53 +0500

    1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
+import streamlit as streamlit
+import pandas as pd
+st.write("""
+        #Weed detection App
+        This app detects the common weed found in the Wheat field
+        """)
 
+st.selectbox('Select Your Province', ["Punjab","Sindh","Balochistan", "KPK", "Azad Jammu and Kashmir", "Gilgit-Baltistan"])
+st.camera_input("Please take the image of the field.")
+st.write("or")
+st.file_uploader('Upload the image of the field', type=['jpg','png','jpeg'])
