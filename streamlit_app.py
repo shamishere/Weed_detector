2646bff949fa0e1bd8e0465b8604226f748e9631
ShameenAnwar <sham17anwar@gmail.com>
2024-05-08 01:16:57 +0500

    2.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as streamlit
 import pandas as pd
-st.write("""
-        #Weed detection App
-        This app detects the common weed found in the Wheat field
-        """)
+st.title('Weed Detection App')
+st.write("This app detects the common weed found in the Wheat fields of Pakistan.")
 
 st.selectbox('Select Your Province', ["Punjab","Sindh","Balochistan", "KPK", "Azad Jammu and Kashmir", "Gilgit-Baltistan"])
 st.camera_input("Please take the image of the field.")
