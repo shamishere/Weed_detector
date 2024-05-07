0d9e4a3af26eadc098c4d1f3182f254a60bfaec2
ShameenAnwar <sham17anwar@gmail.com>
2024-05-08 01:26:49 +0500

    corrected import streamlit as st

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as streamlit
+import streamlit as st
 import pandas as pd
 st.title('Weed Detection App')
 st.write("This app detects the common weed found in the Wheat fields of Pakistan.")
